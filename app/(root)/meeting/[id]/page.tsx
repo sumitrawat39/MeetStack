9722a79e5d84effc5e84f5315c20ba4533d2bf27
sumit rawat <nirnay777@gmail.com>
2026-07-14 11:54:44 +0530

    steup meeting components

--- app/(root)/meeting/[id]/page.tsx
+++ app/(root)/meeting/[id]/page.tsx
@@ -1,11 +1,39 @@
-const Meeting = async ({
-  params,
-}: {
-  params: Promise<{ id: string }>;
-}) => {
-  const { id } = await params;
+"use client";
 
-  return <div>Meeting Room: #{id}</div>;
+import Loader from "@/components/Loader";
+import MeetingRoom from "@/components/MeetingRoom";
+import MeetingSetup from "@/components/MeetingSetup";
+import { useGetCallById } from "@/hooks/useGetCallById";
+import { useUser } from "@clerk/nextjs";
+import { StreamCall, StreamTheme } from "@stream-io/video-react-sdk";
+import { useState } from "react";
+import { useParams } from "next/navigation";
+
+const Meeting = () => {
+  const { id } = useParams<{ id: string }>();
+
+  const { isLoaded } = useUser();
+  const [isSetupComplete, setIsSetupComplete] = useState(false);
+
+  const { call, isCallLoading } = useGetCallById(id);
+
+  if (!isLoaded || isCallLoading) return <Loader />;
+
+  if (!call) return <div>Call not found</div>;
+
+  return (
+    <main className="h-screen w-full">
+      <StreamCall call={call}>
+        <StreamTheme>
+          {!isSetupComplete ? (
+            <MeetingSetup setIsSetupComplete={setIsSetupComplete} />
+          ) : (
+            <MeetingRoom />
+          )}
+        </StreamTheme>
+      </StreamCall>
+    </main>
+  );
 };
 
 export default Meeting;
